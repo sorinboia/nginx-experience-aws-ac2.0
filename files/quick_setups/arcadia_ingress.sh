02b4cb084b9132ea7db3b03b68431afb7b61658c
Sorin Boiangiu <sorinboia@gmail.com>
2021-05-23 09:02:23 +0300

    lic

--- files/quick_setups/arcadia_ingress.sh
+++ files/quick_setups/arcadia_ingress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl apply -f files/4ingress/1arcadia_deploy.yaml
+kubectl apply -f files/4ingress/1arcadia_delpoy.yaml
 kubectl apply -f files/4ingress/1arcadia_increase.yaml
 ./files/4ingress/ingress_install.sh
 
